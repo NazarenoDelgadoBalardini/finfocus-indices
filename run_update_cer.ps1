0d4d26d2bd2a0af3393c7bc082b97a46a5d298ae
NazarenoDelgadoBalardini <nazareno.finfocus@gmail.com>
2025-07-21 18:11:33 -0300

    Delete run_update_cer.ps1

--- run_update_cer.ps1
+++
@@ -1,9 +0,0 @@
-# run_update_indices.ps1
-
-# 1) Instalar dependencias (solo la primera vez o cuando cambies)
-& 'C:\Program Files\Python313\python.exe' -m pip install --upgrade requests beautifulsoup4
-
-# 2) Ejecutar los scripts de actualización
-& 'C:\Program Files\Python313\python.exe' 'C:\Users\Usuario\Documents\finfocus-indices\update_cer.py'
-
-Write-Host "✅ Actualización completada: $(Get-Date -Format o)"
